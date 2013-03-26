075e81a3ac1a1bc85b95509c92070f5e89b613ab
victor cavalcante <victor@cavalcante.net>
2013-03-26 13:51:00 -0300

    incluindo exemplos do bb em um arquivo externo de js

---
+++ AnunciosWebAPI/Scripts/exemplosBB.js
@@ -0,0 +1,23 @@
+﻿//anunciante 1
+var Model = Backbone.Model.extend({ urlRoot: "/api/anunciante", idAttribute: 'IdAnunciante' });
+var model = new Model({ idAttribute: 1 });
+model.fetch();
+
+//salvar novo anunciante
+var model = new Model({ Nome: "Victor Cavalcante", "Email": "vcavalcante@lambda3.com.br" });
+model.save();
+
+//definindo o default
+var Model = Backbone.Model.extend({ urlRoot: "/api/anunciante", idAttribute: 'IdAnunciante', defaults: { Nome: 'João ninguem' } });
+var model = new Model({ "Email": "vcavalcante@lambda3.com.br" });
+model.get("Nome");
+
+//criando collection
+var Anunciante = Backbone.Model.extend({ idAttribute: 'IdAnunciante' });
+var Anunciantes = Backbone.Collection.extend({ model: Anunciante, url: '/api/anunciante' });
+var anunciantes = new Anunciantes();
+anunciantes.fetch()
+anunciantes.toJSON()
+
+//where
+anunciantes.where({ Nome: 'Maurice' })
