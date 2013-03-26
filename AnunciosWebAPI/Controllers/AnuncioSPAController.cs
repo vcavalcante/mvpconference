655a80fb29efc925a4d00222379fa80b2a536c53
victor cavalcante <victor@cavalcante.net>
2013-03-26 12:38:45 -0300

    adicionado o controler da SPA e incluido os pacotes e referencias para o Backbone

---
+++ AnunciosWebAPI/Controllers/AnuncioSPAController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnunciosWebAPI.Controllers
+{
+    public class AnuncioSPAController : Controller
+    {
+        //
+        // GET: /AnuncioSPA/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
